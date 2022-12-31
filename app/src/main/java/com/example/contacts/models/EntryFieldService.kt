9e4aa32280a1151221431dcf65454a40866f2651
Егор Шкляревский <egor39546@gmail.com>
2022-12-31 14:00:41 +0300

    Debug EntryFiledService

--- app/src/main/java/com/example/contacts/models/EntryFieldService.kt
+++ app/src/main/java/com/example/contacts/models/EntryFieldService.kt
@@ -8,13 +8,13 @@ class EntryFieldService{
     fun checkField(context: FragmentActivity,name: EditText, phone: EditText):Boolean{
         var field = false
 
-        if ("$name"!= ""&& "$phone"!= ""){
+        if ("${name.text}"!= ""&& "${phone.text}"!= ""){
             field = true
-        } else if ("$name"== ""&& "$phone"== ""){
+        } else if ("${name.text}"== ""&& "${phone.text}"== ""){
             Toast.makeText(context,"Please, enter name and phone number", Toast.LENGTH_SHORT).show()
-        } else if ("$phone"== ""){
+        } else if ("${phone.text}"== ""){
             Toast.makeText(context,"Please, enter phone number", Toast.LENGTH_SHORT).show()
-        } else if ("$name"== ""){
+        } else if ("${name.text}"== ""){
             Toast.makeText(context,"Please, enter name", Toast.LENGTH_SHORT).show()
         }
 
